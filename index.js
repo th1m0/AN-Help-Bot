c61d8ff4d8fe14b2dbdcc1210ba90909b787fb8c
th1m0 <52822112+th1m0@users.noreply.github.com>
2019-12-19 21:43:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -211,7 +211,7 @@ bot.on('message', message=>{
     
     if(!message.content.startsWith(prefix)) return;
     if(cooldown.has(message.author.id)) {
-        return message.reply("This command has a 30 second cooldown!")
+        return message.author("This command has a 30 second cooldown!")
     }
     if(message.channel.name === 'bot-commands')
     cooldown.add(message.author.id);
