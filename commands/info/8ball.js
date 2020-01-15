c4dafae21ed2a514fb4ba24db055574e272b2240
th1m0 <52822112+th1m0@users.noreply.github.com>
2020-01-15 22:41:33 +0100

    Update 8ball.js

--- commands/info/8ball.js
+++ commands/info/8ball.js
@@ -6,8 +6,9 @@ module.exports = {
         const Discord = require("discord.js");
         
         let botchannel = message.guild.channels.find(`name`, "bot-commands")
-        if(message.channel.name === 'bot-commands' || 'memes') {
-
+        let memeChannel = message.guild.channels.find(`name`, "memes")
+        if(message.channel.name === 'bot-commands'){
+        
         if(!args[1]) return message.reply("Please ask a full question!");
 let replies = ["Yes.", "No.", "I don't know.", "Ask again later", "It is certain", "It is decidedly so", "Without a doubt", "definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Signs point to yes", "Reply hazy, try again", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"];                                            
 
@@ -23,8 +24,29 @@ let ballembed = new Discord.RichEmbed()
 .setFooter("Arkhamnetwork | Thiimo#6969");
 
 message.channel.send(ballembed)
-        }else{
-            message.channel.send("This command can only be used in #bot-commands or $memes").then(m => m.delete(3000))
+        }else if(message.channel.name === 'memes'){
+            if(!args[1]) return message.reply("Please ask a full question!");
+            let replies = ["Yes.", "No.", "I don't know.", "Ask again later", "It is certain", "It is decidedly so", "Without a doubt", "definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Signs point to yes", "Reply hazy, try again", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"];                                            
+            
+            let result = Math.floor((Math.random() * replies.length));
+            let question = args.slice(0).join(" ");
+            
+            let ballembed = new Discord.RichEmbed()
+            .setAuthor(message.author.tag)
+            .setColor("#FF9900")
+            .addField("Question", question)
+            .addField("Answer", replies[result])
+            .setThumbnail("https://www.cafecremers.nl/wp-content/uploads/2019/03/8ball.jpg")
+            .setFooter("Arkhamnetwork | Thiimo#6969");
+            
+            message.channel.send(ballembed)
         }
+        
+        
+        
+        else{
+            message.channel.send(`This command can only be used in ${botchannel} or ${memeChannel}`).then(m => m.delete(3000))
+        }
+   
     }
 }
