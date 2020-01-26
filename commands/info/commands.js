61d30e68848d402fe2d4406161ece8c6b5a9682b
th1m0 <52822112+th1m0@users.noreply.github.com>
2020-01-26 02:20:32 +0100

    Update commands.js

--- commands/info/commands.js
+++ commands/info/commands.js
@@ -1,3 +1,23 @@
+module.exports = {
+    name: "commands",
+    aliases: ["cmds"],
+    description: "Returns all commands, or one specific command info",
+    category: "info",
+    run: async (bot, message, args) => {
+      const Discord = require("discord.js");
+      
+      
+        let embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setDescription("**Commands:**\nhelp\n8ball\nmeme\nmcstatus\n\n**Prison commands:**\nbackpacks\ncalculate")
 
+        message.author.send(embed);
+        message.channel.send("Send you a message in DM").then(m => m.delete(5000))
+        .catch(err => console.log(err));
+      
 
-
+    
+  
+      
+    }
+  }
